373e686569f3e4631bfa7cb4990bbc5d70f7889d
Jay <jay.boyarsky@gmail.com>
2015-03-20 22:44:20 -0400

    updated run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,12 @@
 # Step 1 
 # Read all data sets to create  one data set 
-############################################################################### 
+
 library(plyr)
 library(dplyr)
-##Step 0 read all data sets 
-dir <- "C:/coursera/cleandata/UCI HAR Dataset" 
+##Step 0 read all data sets-  information only 
+#dir <- "E:/coursera/cleandata/UCI HAR Dataset" 
+dir <- "E:/coursera/UCI HAR Dataset"
+
 x_train <- read.table(paste(dir,"/train/X_train.txt", sep ="/" ))
 y_train <- read.table(paste(dir, "/train/y_train.txt", sep ="/")) 
 subject_train <- read.table(paste(dir, "/train/subject_train.txt", sep = "/")) 
@@ -17,26 +19,22 @@ activities <- read.table(paste(dir, "activity_labels.txt", sep = "/"))
 ## Step 1 Merge Training and testing data sets
 x_tt <- rbind(x_train, x_test)
 y_tt <- rbind(y_train, y_test)
-
-# Step 3.Uses descriptive activity names to name the activities in the data set
 subject_tt <- rbind(subject_train, subject_test) 
+
+## Step 2.Uses descriptive activity names to name the activities in the data set
 names(x_tt) <- features[ ,2]
 
-## Step 2 Extract the mean and standard deviation for each measurement 
+## Step 3 Extract the mean and standard deviation for each measurement 
 x_tt_mn_std <- grep("(mean|std)\\(\\)", names(x_tt))
 select_mn_std <- x_tt[, x_tt_mn_std]
+
+##Step 4 appropriatly name data set with descriptive variable names
 y_tt[, 1] <- activities[y_tt[, 1], 2]
 names(y_tt) <- "activity"
-
-# Step 3.Uses descriptive activity names to name the activities in the data set
-subject_tt <- rbind(subject_train, subject_test) 
-names(x_tt) <- features[ ,2]
-
-#Step 4 appropriatly name data set with discriptiv3e d
 names(subject_tt) <- "subject" 
-combined_data <- cbind(y_tt, select_mn_std , subject_tt)
 
 ## Step 5 from step 4 create a data set with the average of each variable for each activity and each subject.
+combined_data <- cbind(y_tt, select_mn_std , subject_tt)
 avg_data <- ddply(combined_data, c("subject","activity"), numcolwise(mean))
 write.table(avg_data, "E:/averages_data.txt", row.name=FALSE) 
 View(avg_data)
